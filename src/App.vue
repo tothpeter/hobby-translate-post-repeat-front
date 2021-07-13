18e3c56ae295d53769d460568a69c72e04e00f36
Peter Toth <tothpeter08@gmail.com>
2021-07-13 11:46:29 -0500

    Add basic bootstrap header

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Nav />
+  <div class="auth-wrapper">
+    <div class="auth-inner">
+      <Home />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Nav from './components/Nav.vue'
+import Home from './components/Home.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Nav,
+    Home
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ * {
+    box-sizing: border-box;
+  }
+
+  body {
+    background: #1c8ef9 !important;
+    min-height: 100vh;
+    display: flex;
+    font-weight: 400;
+  }
+
+  body, html, #app, #root, .auth-wrapper {
+    width: 100%;
+    height: 100%;
+  }
+
+  #app {
+    text-align: center;
+  }
+
+  .auth-wrapper {
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    text-align: left;
+  }
+
+  .auth-inner {
+    width: 450px;
+    margin: auto;
+    background: white;
+    padding: 40px 55px;
+    border-radius: 15px;
+    transition: all .3s;
+  }
+
+  .auth-wrapper .form-control:focus {
+    border-color: #167bff;
+    box-shadow: none;
+  }
 </style>
---
+++ src/components/Nav.vue
@@ -0,0 +1,28 @@
+<template>
+  <nav class="navbar navbar-expand navbar-light fixed-top bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">Home</a>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+            <a class="nav-link" href="#">Login</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Sign up</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Nav'
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
