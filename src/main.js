9e0b7ac1c7a9e7032855bd592f74e23100395b24
Peter Toth <tothpeter08@gmail.com>
2021-07-14 12:17:04 -0500

    Add vuex

--- src/main.js
+++ src/main.js
@@ -3,8 +3,10 @@ import App from './App.vue'
 import router from './router'
 import axios from '@/plugins/axios'
 import session from '@/plugins/session'
+import store from './store'
 
 createApp(App)
+  .use(store)
   .use(axios)
   .use(session)
   .use(router)
