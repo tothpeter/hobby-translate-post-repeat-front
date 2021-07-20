6f05298f002b8b3e7c0b0aff6708aa862e5aa3e7
Peter Toth <tothpeter08@gmail.com>
2021-07-20 13:02:58 -0500

    Redirect authenticated users from login and register routes to home

--- src/router/index.js
+++ src/router/index.js
@@ -5,12 +5,17 @@ import Login from '@/components/Login.vue'
 import Register from '@/components/Register.vue'
 import LatestFeed from '@/components/dashboard/LatestFeed.vue'
 
-import Session from '@/plugins/session.js'
+import Session from '@/plugins/session'
+
+const ifNotAuthenticated = (to, from, next) => {
+  if (!Session.isAuthenticated()) next()
+  else next('/')
+}
 
 const routes = [
   { path: '/', component: Home },
-  { path: '/login', component: Login },
-  { path: '/register', component: Register },
+  { path: '/login', component: Login, beforeEnter: ifNotAuthenticated },
+  { path: '/register', component: Register, beforeEnter: ifNotAuthenticated },
   { path: '/dashboard/latest-feed', component: LatestFeed },
 ]
 
