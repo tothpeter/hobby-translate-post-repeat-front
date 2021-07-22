da3226c2ebef6740e14fd517adde2455be85b4bb
Peter Toth <tothpeter08@gmail.com>
2021-07-21 20:53:35 -0500

    Tune CSS style a bit

--- src/App.vue
+++ src/App.vue
@@ -34,42 +34,38 @@ export default {
 </script>
 
 <style>
- * {
-    box-sizing: border-box;
-  }
+* {
+  box-sizing: border-box;
+}
 
-  body {
-    background: #1c8ef9 !important;
-    padding-top: 60px;
-  }
+body {
+  background: #1c8ef9 !important;
+  padding-top: 60px;
+}
 
-  #app, #root, .auth-wrapper {
-    width: 100%;
-    height: 100%;
-  }
+#app, #root, .auth-wrapper {
+  width: 100%;
+  height: 100%;
+}
 
-  #app {
-    text-align: center;
-  }
+.auth-wrapper {
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  text-align: left;
+}
 
-  .auth-wrapper {
-    display: flex;
-    justify-content: center;
-    flex-direction: column;
-    text-align: left;
-  }
+.auth-inner {
+  width: 450px;
+  margin: auto;
+  background: white;
+  padding: 40px 55px;
+  border-radius: 15px;
+  transition: all .3s;
+}
 
-  .auth-inner {
-    width: 450px;
-    margin: auto;
-    background: white;
-    padding: 40px 55px;
-    border-radius: 15px;
-    transition: all .3s;
-  }
-
-  .auth-wrapper .form-control:focus {
-    border-color: #167bff;
-    box-shadow: none;
-  }
+.auth-wrapper .form-control:focus {
+  border-color: #167bff;
+  box-shadow: none;
+}
 </style>
